0163768e363bcc06787750537f2e9e2960ae4a18
minder <b.bloknot@yandex.ru>
2020-09-06 23:37:37 +0300

    added files

---
+++ AppForGit/AppForGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AppForGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Hello GitHub!");
+        }
+    }
+}
